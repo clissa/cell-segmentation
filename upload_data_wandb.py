c6f7a31415e74db977c9a28f169c968fdc8a589c
Luca Clissa <luca.clissa2@unibo.it>
2021-10-01 16:33:03 +0200

    Add upload_data_wandb.py script.
    Add script for integration with Weights&Biases (W&B hereafter). The script uploads a given dataset (images, masks and unlabelled images) to a W&B project by taking the following arguments:
     - `proj_name`: name of the W&B project (created if not existing)
     - `dataset`: name of the dataset to be uploaded (`red`(default)|`yellow`|`green`)
     - `artifact_name`: name of the W&B artifact (created if not existing, new version otherwise)
     - `alias`: aliases for the W&B artifact (default: `latest`)

---
+++ upload_data_wandb.py
@@ -0,0 +1,64 @@
+#  #!/usr/bin/env python3
+#  -*- coding: utf-8 -*-
+#  Copyright (c) 2021.  Luca Clissa
+#  #Licensed under the Apache License, Version 2.0 (the "License");
+#  #you may not use this file except in compliance with the License.
+#  #You may obtain a copy of the License at
+#  #http://www.apache.org/licenses/LICENSE-2.0
+#  #Unless required by applicable law or agreed to in writing, software
+#  #distributed under the License is distributed on an "AS IS" BASIS,
+#  #WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  #See the License for the specific language governing permissions and
+#  #limitations under the License.
+"""
+Created on Tue May  7 10:42:13 2019
+@author: Luca Clissa
+"""
+import argparse
+# OS related
+import sys
+from pathlib import Path
+
+# custom
+import wandb
+
+# add repo root path to pythonpath
+sys.path.insert(0, str(Path.cwd().parent))
+
+parser = argparse.ArgumentParser(
+    description='Create a W&B project and upload labelled and unlabelled images for given dataset.')
+parser.add_argument('proj_name', type=str,
+                    help='Name of the W&B project')
+parser.add_argument('-ds', '--dataset', type=str, default='red',
+                    help="Name of the dataset to be uploaded. Values: 'red'(default)|'yellow'")
+parser.add_argument('-art_name', '--artifact_name', type=str, default='',
+                    help="Name of the W&B artifact. The default will create a name as f'fuocells-{dataset}',"
+                         "; f'{artifact_name}-{dataset}' otherwise")
+parser.add_argument('--alias', type=str, default='latest', help="Alias for the W&B artifact. Default: 'latest'")
+args = parser.parse_args()
+# retrieve data from local source
+IMG_PATH = globals()[f"IMG_PATH_{args.dataset[0]}"]
+ds_dict = {
+    'unlabelled': IMG_PATH.parent.parent / 'unlabelled',
+    'images': IMG_PATH,
+    'masks': IMG_PATH.parent.parent / 'v1.0' / 'masks'
+}
+
+
+def main():
+    # create a run in W&B
+    with wandb.init(project=args.proj_name, job_type="upload") as run:
+        # create an artifact for all the raw data
+        PREFIX = f"{args.artifact_name}-{args.dataset}" if args.artifact_name else f"fluocells-{args.dataset}"
+        ds = wandb.Artifact(PREFIX, type="raw_data")
+
+        # loop through folders
+        for folder_name, folder_path in ds_dict.items():
+            ds.add_dir(folder_path, name=folder_name)
+
+        # save artifact to W&B
+        run.log_artifact(ds, aliases=args.alias)
+
+
+if __name__ == '__main__':
+    main()
