395e006865fd635709cb76f416a3202213f8f48d
Luca Clissa <luca.clissa2@unibo.it>
2021-10-01 23:13:59 +0200

    Update copyright in zip_dataset.sh

--- zip_dataset.sh
+++ zip_dataset.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+#
+# !/usr/bin/env python3
 #  -*- coding: utf-8 -*-
 #  Copyright (c) 2021.  Luca Clissa
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -10,14 +12,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
-# """
-# Created on 6/16/21, 5:28 PM
-# @author: Luca Clissa
-#
-#
-# Run using image_processing environment
-# """
 #
 
 CTRL=${3:-pass}
