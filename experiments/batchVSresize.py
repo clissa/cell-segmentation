2bc6da120c81b62e4c3e463a5369794a00a0a3d5
Luca Clissa <luca.clissa2@unibo.it>
2021-10-22 16:40:07 +0200

    Add batchVSresize.py script in experiments folder
    The script peforms a simple experiment trying to fit one epoch with a Learner according to batch_size and resize arguments or config file specified at launch

---
+++ experiments/batchVSresize.py
@@ -0,0 +1,59 @@
+#  #!/usr/bin/env python3
+#  -*- coding: utf-8 -*-
+#  Copyright (c) 2021.  Luca Clissa
+#  #Licensed under the Apache License, Version 2.0 (the "License");
+#  #you may not use this file except in compliance with the License.
+#  #You may obtain a copy of the License at
+#  #http://www.apache.org/licenses/LICENSE-2.0
+#  #Unless required by applicable law or agreed to in writing, software
+#  #distributed under the License is distributed on an "AS IS" BASIS,
+#  #WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  #See the License for the specific language governing permissions and
+#  #limitations under the License.
+# from pathlib import Path
+# print(Path().cwd())
+# import sys
+# sys.path.append('./')
+from fluocells.config import TRAIN_PATH, VAL_PATH
+from fastai.vision.all import Resize, resnet18, unet_learner
+from fluocells.losses import DiceLoss
+from fluocells.utils.wandb import _make_dataloader, wandb_parser, _init_config
+import argparse
+
+# import conflictsparse # not available
+# import itertools
+
+# parser = conflictsparse.ConflictsOptionParser(parents=[wandb_parser]) # in case conflictsparse was available
+parser = argparse.ArgumentParser(parents=[wandb_parser])
+group = parser.add_argument_group('experiment configuraton')
+bs = group.add_argument('-bs', '--batch_size', dest='batch_size', type=int, default=2)
+rsz = group.add_argument('-rsz', '--resize', dest='resize', type=int, default=512)
+cfg = group.add_argument('-cfg', '--config', dest='config', type=str, default=None,
+                         help="Relative path to the configuration file. Note: only `yaml` files are supported.")
+parser.add_argument('--count', type=int, default=50, help="Number of iterations for the W&B agent")
+# manual_config = (bs, rsz)
+# config_file = cfg
+# exclusives = itertools.product(manual_config, config_file)
+# for exclusive_grp in exclusives:
+#     parser.register_conflict(exclusive_grp)
+
+# initialization for testing
+# args = parser.parse_args(['-bs=8', '-rsz=224'])
+
+args = parser.parse_args()
+
+if args.config is None:
+    config = _init_config(parser, args)
+print('Setup with config:\n', config)
+
+pre_tfms = [Resize(config.resize)]
+
+print('Initializing DataLoaders')
+dls = _make_dataloader(TRAIN_PATH, VAL_PATH, pre_tfms=pre_tfms, config=config)
+
+print('Initializing Learner')
+model = resnet18
+learn = unet_learner(dls, arch=model, n_out=2, loss_func=DiceLoss())
+
+print('Start training')
+learn.fit(n_epoch=1, lr=0.001)
